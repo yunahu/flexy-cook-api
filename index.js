679dd7f27b19a96105e395bf183069c8b8037ab9
Fuka Nagata <fleur0121@icloud.com>
2024-02-29 23:19:34 -0800

    added spoonacular backend

--- index.js
+++ index.js
@@ -12,11 +12,56 @@ app.post('/todoist_token', async (req, res) => {
 	try {
 		const code = req.body.code;
 		const response = await axios.post(`https://todoist.com/oauth/access_token?client_id=${process.env.TODOIST_CLIENT_ID}&client_secret=${process.env.TODOIST_CLIENT_SECRET}&code=${code}`);
-		
+
 		res.send(response.data.access_token);
 	} catch (err) {
 		console.error(err.message);
 	}
 });
+
+const spoonacularAPI = axios.create({
+	baseURL: 'https://api.spoonacular.com/recipes'
+});
+
+spoonacularAPI.interceptors.request.use(config => {
+	config.headers['x-api-key'] = process.env.SPOONACULAR_CLIENT_ID;
+	return config;
+},
+	error => Promise.reject(error)
+);
+
+spoonacularAPI.interceptors.response.use(response => response.data,
+	error => Promise.reject(error)
+);
+
+app.get('/test', async (req, res) => {
+	res.status(200).send('hello!');
+})
+
+app.get('/spoonacular/searchRecipe', async (req, res) => {
+	try {
+		const data = await spoonacularAPI.get('/complexSearch', { params: req.query });
+		res.status(200).json(data);
+	} catch (err) {
+		console.log(err);
+		res.status(400).send(err);
+	}
+})
+
+app.get('/spoonacular/getRecipe', async (req, res) => {
+	if (req.query.id === undefined || req.query.includeNutrition === undefined) {
+		res.status(401).send('Not enough params');
+		return;
+	}
+
+	try {
+		const data = await spoonacularAPI.get(`/${req.query.id}/information?includeNutrition=${req.query.includeNutrition}`, { params: req.query });
+		res.status(200).json(data);
+	} catch (err) {
+		console.log(err);
+		res.status(400).send(err);
+	}
+})
+
 
 app.listen(process.env.PORT, () => console.log(`Listening on http://localhost:${process.env.PORT}`));
